cbd2a2123446f6912d8c1c0c528e9a20569f0687
iamsaturn <arthusjonathan@gmail.com>
2026-08-12 01:18:59 +0200

    testing with schemas

--- mynewapi/mynewapi/app.py
+++ mynewapi/mynewapi/app.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
 from http import HTTPStatus
-from mynewapi.schemas import Message
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from mynewapi.schemas import Message, HelloWorldHTML
 
 app = FastAPI()
 
 
-@app.get('/', status_code=HTTPStatus.OK, response_model= Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'try'}
+    return {'message': 'Hello World'}
+
+
+@app.get('/htmlhello', status_code=HTTPStatus.OK,
+         response_class=HTMLResponse,
+         response_model= HelloWorldHTML)
+def hello_world():
+    return '<h1>Olá Mundo</h1>'
--- mynewapi/tests/test_app.py
+++ mynewapi/tests/test_app.py
@@ -13,3 +13,10 @@ def test_root_should_return_hello_world():
 
     assert response.json() == {'message': 'Hello World'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_html_hello_should_return_html_hello_world():
+
+    client = TestClient(app)
+    response = client.get('/htmlhello')
+    assert response.text == '<h1>Olá Mundo</h1>'
